d6fb4b82fd303b7bc43a045382bbdb74429b0a96
Allan Perry <alperry21@gmail.com>
2024-07-15 22:29:30 -0600

    checks

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -13,28 +13,42 @@ export default class App extends React.Component {
     const { value } = evt.target
     this.setState({ ...this.state, todoNameInput: value })
   }
+
+  resetForm = () => this.setState({ ...this.state, todoNameInput: ''})
+
+  setAxiosResponseError = err => this.setState({ ...this.setState, error: err.response.data.message })
+  
   postNewTodo = () => {
     axios.post(URL, { name: this.state.todoNameInput })
       .then(res => {
-        this.fetchAllTodos()
-        this.setState({ ...this.state, todoNameInput: '' })
+        this.setState({ ...this.state, todos: this.state.todos.concat(res.data.data) })
+        this.resetForm()
       })
-      .catch(err => {
-        this.setState({ ...this.setState, error: err.response.data.message })
-      })
+      .catch(this.setAxiosResponseError)
   }
+
   fetchAllTodos = () => {
     axios.get(URL)
       .then(res => {
         this.setState({ ...this.state, todos: res.data.data })
       })
-      .catch(err => {
-        this.setState({ ...this.setState, error: err.response.data.message })
-      })
+      .catch(this.setAxiosResponseError)
   }
   onTodoFormSubmit = evt => {
     evt.preventDefault()
     this.postNewTodo()
+  }
+  toggleCompleted = id => () => {
+    axios.patch(`${URL}/${id}`)
+      .then(res => {
+        this.setState({ 
+          ...this.state, todos: this.state.todos.map(td => {
+            if (td.id !== id) return td
+            return res.data.data
+        })
+      })
+      })
+      .catch(this.setAxiosResponseError)
   }
   componentDidMount() {
     //fetch all todos from server
@@ -48,7 +62,7 @@ export default class App extends React.Component {
         <h2>Todos:</h2>
         {
           this.state.todos.map(td => {
-            return <div key={td.id}>{td.name}</div>
+            return <div onClick={this.toggleCompleted(td.id)} key={td.id}>{td.name}{td.completed ? ' ✔' : ''} </div>
           })
         }
       </div>
